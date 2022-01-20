9498266832aa7729d0cdcd3a94d3a870a89319b0
tunde99 <tundeajayi@outlook.com>
2022-01-20 02:08:52 +0100

    add PlusMinus

--- Hackerrank/plus-minus.py
+++ Hackerrank/plus-minus.py
@@ -6,6 +6,12 @@ import random
 import re
 import sys
 
+'''
+Given an array of integers, calculate the ratios of its elements 
+are positive, negative, and zero. Print the decimal value of each 
+fraction on a new line with  places after the decimal.
+'''
+
 #
 # Complete the 'plusMinus' function below.
 #
@@ -13,19 +19,22 @@ import sys
 #
 
 def plusMinus(arr):
-    # Write your code here
+    """
+    input arr: input array of fixed length
+    n: length of the array
+    """
     pos = 0
     neg = 0
     zero = 0
+    
     for num in arr:
         if num > 0:
             pos += 1
-            # print()
         if num < 0:
             neg += 1
-            # print()
         if num == 0:
             zero += 1
+
     print(f'{pos/n:.6f}')
     print(f'{neg/n:.6f}')
     print(f'{zero/n:.6f}')
